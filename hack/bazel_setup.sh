d6d5f1c191553b0a8a2a9309f921ebdb400cc98e
Mustafa Gezen <mustafa@ctrliq.com>
2022-10-30 06:55:36 +0100

    Use remote downloader during fetch

--- hack/bazel_setup.sh
+++ hack/bazel_setup.sh
@@ -4,13 +4,15 @@ export SILO_KEY=rocky86-peridot-prow-1
 export REMOTE_DEF=cache-silo-key=$SILO_KEY
 
 CI=${CI:-}
+QUERY_FLAGS=""
 FLAGS="--show_progress_rate_limit=5 --color=yes --ui_actions_shown=30 --terminal_columns=140 --show_timestamps --verbose_failures --announce_rc --experimental_repository_cache_hardlinks --disk_cache= --sandbox_tmpfs_path=/tmp --experimental_guard_against_concurrent_changes"
 
 if [[ -n ${CI} ]]; then
   FLAGS="--config=ci $FLAGS"
+  QUERY_FLAGS="--config=ci"
 fi
 
 BAZEL_B="bazel $SOPTIONS build $FLAGS --remote_default_exec_properties=$REMOTE_DEF"
 BAZEL_R="bazel $SOPTIONS run $FLAGS --remote_default_exec_properties=$REMOTE_DEF"
 BAZEL_T="bazel $SOPTIONS test $FLAGS --remote_default_exec_properties=$REMOTE_DEF --test_arg=-test.v --flaky_test_attempts=3 --build_tests_only --test_output=errors"
-BAZEL_QR="bazel $SOPTIONS query --keep_going --noshow_progress"
+BAZEL_QR="bazel $SOPTIONS query $QUERY_FLAGS --keep_going --noshow_progress"
