343a8f9ce4c08b1222c89c21fc21e03f8f1f2a5d
Mustafa Gezen <mustafa@ctrliq.com>
2022-10-30 06:20:56 +0100

    Disable set -x and fix BAZEL_QR

--- hack/non_manual_build.sh
+++ hack/non_manual_build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -x
 
 source hack/bazel_setup.sh
 
-$BAZEL_B $(BAZEL_QR query "//... except attr(tags, 'manual', //...) except //vendor/...")
+$BAZEL_B $($BAZEL_QR query "//... except attr(tags, 'manual', //...) except //vendor/...")
--- hack/unit_tests.sh
+++ hack/unit_tests.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -x
 
 source hack/bazel_setup.sh
 
